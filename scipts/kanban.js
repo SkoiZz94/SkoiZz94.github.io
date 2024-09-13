e898dfd79cc3f4c5c400ccec7236a9e610c8c8ae
Rui Amoroso (CTW) <rui.amoroso@ctw.bmwgroup.com>
2024-09-13 20:22:22 +0100

    update structure

---
+++ scipts/kanban.js
@@ -0,0 +1,5 @@
+// Kanban functionality script placeholder
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Kanban board script loaded");
+    // Add functionality here for moving tasks between columns, adding/removing tasks, etc.
+});
